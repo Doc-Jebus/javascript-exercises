d83237b7a718487c3659184202275deaa1acddd3
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-06-28 13:03:20 +0900

    Completed the array task.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -3,12 +3,10 @@ const reverseString = function(str) {
     let reverseArray = splitString.reverse();
     let joinArray = reverseArray.join("");
     return joinArray;
+    
 };
 
-reverseString('hello');
-reverseString('hello there');
-reverseString('123! abc!');
-reverseString('');
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = [];
+    //The 'for' loop, used by the 'forEach' method, includes a function with the callback of "item". If an array item is seen a second time, it will break out of the loop (triggered by the bang mark '!arg'), continue with pushing the remainder of the new array list. 
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
+        console.log(newArray);
+      }
+    })
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
