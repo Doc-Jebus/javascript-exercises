636ed22f2c362126c120efdf0dbba760816e76fd
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-06-28 13:57:36 +0900

    In the process of completing exercise 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    const arr = [];
+    while(start <= end) {
+        arr.push(start++);
+    }
+    
 };
+ sumAll(1, 10);
+ console.log(arr);
 
 // Do not edit below this line
 module.exports = sumAll;
