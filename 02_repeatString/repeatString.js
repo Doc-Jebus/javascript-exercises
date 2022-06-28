7c86f2028c438d87f79f79f06f4c1bb574de77d2
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-06-28 09:26:36 +0900

    Exercise 2- repeatString completed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
-for()
+function repeatString(string, num) {
+  if(num > 0) {
+  return string.repeat(num);
+  } else if (num < 0) {
+    return "ERROR";
+  } else {
+    return "";
+  }
+  
 };
+const number = Math.floor(Math.random() * 1000);
+repeatString('hey', 3);
+
+repeatString('hey', 10);
+
+repeatString('hey', 1);
+
+repeatString('hey', 0);
+repeatString('hey', -1);
+repeatString('hey', number);
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
