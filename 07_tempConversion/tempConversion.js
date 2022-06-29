309fe4a4f331f73dd4d45982acfccc5eccce6044
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-06-29 17:35:40 +0900

    Completed exercise 06

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(tempFah) {
+  let convert = (tempFah - 32) * 0.5556;
+  convert = +convert.toFixed(1);
+  console.log(convert);
+  
+  return convert;
 };
 
-const ctof = function() {
-
+const ctof = function(tempCel) {
+  let convert = (tempCel * 1.8) + 32;
+  convert = +convert.toFixed(1);
+  console.log(convert);
+  
+  return convert;
 };
+
 
 // Do not edit below this line
 module.exports = {
