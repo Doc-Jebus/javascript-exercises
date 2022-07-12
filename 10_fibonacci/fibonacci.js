cf3867570230098df8e71d0052cfe8d392890be5
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-07-13 01:55:28 +0900

    Fibonacci challenge completed.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(fib) {
+    let fibs = [1, 1];
+    let sum = 0;
+    for (let i = 0; i < fib; i++) {
+        sum = fibs.at(-1) + fibs.at(-2);
+        fibs.push(sum);
+    };
+    if (fib >= 0) {
+        return fibs[fib-1];
+    } else if (fib < 0) {
+        return "OOPS";
+    };
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
