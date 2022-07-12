6823eed12c5b114a067e4616b5d73e62423c7899
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-07-13 02:26:59 +0900

    getTheTitles challenge complete.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,18 @@
-const getTheTitles = function() {
+const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
 
+const getTheTitles = (books)=> {
+    let result = books.map(book => book.title);
+    console.log(result);
+    return result;
 };
 
 // Do not edit below this line
